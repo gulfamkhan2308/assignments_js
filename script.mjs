027ad5ff2f818994e32ccd39ee311c147e871594
gulfamkhan2308 <sohailahmadkhan2308@gmail.com>
2023-11-01 09:41:19 -0700

    glass applied

--- script.mjs
+++ script.mjs
@@ -1,14 +1,8 @@
-
-const convertCelsiusToFahrenheit = (celsius) => {
-return (celsius * 9/5) + 32;
-};
-
-document.getElementById("convertButton").addEventListener("click", () => {
-const celsiusInput = parseFloat(document.getElementById("celsiusInput").value);
-if (!isNaN(celsiusInput)) {
- const fahrenheitResult = convertCelsiusToFahrenheit(celsiusInput);
- document.getElementById("fahrenheitResult").textContent = fahrenheitResult.toFixed(2);
-} else {
- alert("Please enter a valid number for Celsius temperature.");
+function tempcon(event){
+    console.log("button click");
+    event.preventDefault();
+    let input = document.querySelector("#celsiusInput").value;
+    let result = (input * 9/5) + 32
+    let massage = ""+ result;
+    document.querySelector("#fahrenheitResult").innerHTML= massage;
 }
-});
